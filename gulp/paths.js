1144f122b689499f456dcbd90acab2b63cee01be
Vold-Vacuit <vold-vacuit@gmail.com>
2016-03-02 00:51:09 +0300

    fixes

---
+++ gulp/paths.js
@@ -0,0 +1,11 @@
+export default {
+	appImages: 'app/images',
+	appStylesHelpers: 'app/styles/helpers',
+	appGrid: 'app/styles/',
+	dist: 'dist',
+	assets: 'dist/assets/',
+	images: 'dist/assets/images',
+	scripts: 'dist/assets/scripts',
+	styles: 'dist/assets/styles',
+	grid: 'dist/assets/styles'
+};
